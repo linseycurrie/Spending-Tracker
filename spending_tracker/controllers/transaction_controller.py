ad2e443486e6d4be4a23fe9146dccb6ac9fbbc23
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2020-09-29 10:14:07 +0100

    Revert "Working App"
    This reverts commit bcbad67f05b67e5395d28a8791d429911e55fd8b.

--- spending_tracker/controllers/transaction_controller.py
+++ spending_tracker/controllers/transaction_controller.py
@@ -14,15 +14,13 @@ def transactions():
     total = transaction_repository.total_amount(all_transactions)
     transactions = transaction_repository.sort_transactions(all_transactions)
     categorys = category_repository.select_all()
-    users = user_repository.select_all()
-    return render_template("transactions/index.html", transactions=transactions, total=total, categorys=categorys, users=users)
+    return render_template("transactions/index.html", transactions=transactions, total=total, categorys=categorys)
 
 @transactions_blueprint.route("/transactions/new", methods=['GET'])
 def new_transaction():
     merchants = merchant_repository.select_all()
     categorys = category_repository.select_all()
-    users = user_repository.select_all()
-    return render_template("transactions/new.html", merchants=merchants, categorys=categorys, users=users)
+    return render_template("transactions/new.html", merchants=merchants, categorys=categorys)
 
 @transactions_blueprint.route("/transactions", methods=['POST'])
 def create_transactions():
@@ -30,19 +28,21 @@ def create_transactions():
     category    = category_repository.select(request.form['category_id'])
     date        = request.form['date']
     merchant    = merchant_repository.select(request.form['merchant_id'])
-    user        = user_repository.select(request.form['user_id'])
-    transaction = Transaction(amount, category, date, merchant, user)
+    transaction = Transaction(amount, category, date, merchant)
     transaction_repository.save(transaction)
     return redirect("/transactions")
 
+# @transactions_blueprint.route("/transactions/<id>", methods=['GET'])
+# def show_transaction(id):
+#     transactions = transaction_repository.select(id)
+#     return render_template("transactions/show.html", transactions=transactions)
 
 @transactions_blueprint.route("/transactions/<id>/edit", methods=['GET'])
 def edit_transaction(id):
     transaction = transaction_repository.select(id)
     merchants = merchant_repository.select_all()
     categorys = category_repository.select_all()
-    users = user_repository.select_all()
-    return render_template("transactions/edit.html", transaction=transaction, merchants=merchants, categorys=categorys, users=users)
+    return render_template("transactions/edit.html", transaction=transaction, merchants=merchants, categorys=categorys)
 
 @transactions_blueprint.route("/transactions/<id>", methods=['POST'])
 def update_transaction(id):
@@ -50,8 +50,8 @@ def update_transaction(id):
     category = category_repository.select(request.form['category_id'])
     date = request.form['date']
     merchant = merchant_repository.select(request.form['merchant_id'])
-    user = user_repository.select(request.form['user_id'])
-    transaction = Transaction(amount, category, date, merchant, user, id)
+    
+    transaction = Transaction(amount, category, date, merchant, id)
     transaction_repository.update(transaction)
     return redirect("/transactions")
 
@@ -65,24 +65,12 @@ def filter_month():
     filter = request.form['filter']
     filtered_transactions = transaction_repository.filter_by_month(filter)
     categorys = category_repository.select_all()
-    users = user_repository.select_all()
-    total = transaction_repository.total_amount(filtered_transactions)
-    return render_template("transactions/index.html", transactions=filtered_transactions, categorys=categorys, users=users, total=total)
+    return render_template("transactions/index.html", transactions=filtered_transactions, categorys=categorys)
 
 @transactions_blueprint.route("/transactions/filteredcategory", methods=['POST'])
 def filter_category():
     filter = request.form['filter']
     filtered_transactions = transaction_repository.filter_by_category(filter)
     categorys = category_repository.select_all()
-    users = user_repository.select_all()
     total = transaction_repository.total_amount(filtered_transactions)
-    return render_template("transactions/index.html", transactions=filtered_transactions, categorys=categorys, users=users, total=total)
-
-@transactions_blueprint.route("/transactions/filtereduser", methods=['POST'])
-def filter_name():
-    filter = request.form['filter']
-    filtered_transactions = transaction_repository.filter_by_name(filter)
-    users = user_repository.select_all()
-    categorys = category_repository.select_all()
-    total = transaction_repository.total_amount(filtered_transactions)
-    return render_template("transactions/index.html", transactions=filtered_transactions, categorys=categorys, users=users, total=total)
+    return render_template("transactions/index.html", transactions=filtered_transactions, categorys=categorys, total=total)
--- spending_tracker/models/transaction.py
+++ spending_tracker/models/transaction.py
@@ -1,10 +1,9 @@
 
 class Transaction:
 
-    def __init__(self, amount, category, date, merchant, user, id=None):
+    def __init__(self, amount, category, date, merchant, id=None):
         self.amount = amount
         self.category = category
         self.date = date
         self.merchant = merchant
-        self.user = user
         self.id = id
