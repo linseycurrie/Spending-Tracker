372ef0d0464f528b0d38954d83095acdd04dabc7
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2020-09-29 14:47:34 +0100

    Some imporved CSS

--- spending_tracker/tests/user_test.py
+++ spending_tracker/tests/user_test.py
@@ -6,11 +6,3 @@ class TestTask(unittest.TestCase):
     def setUp(self):
         self.user = User("John", 20.00, 30,00)
 
-    def test_user_has_name(self):
-        self.assertEqual("John", self.user.name)
-
-    def test_user_can_set_spending_limit(self):
-        self.assertEqual(20.00, self.user.spending_limit)
-
-    def test_user_can_set_savings_goal(self):
-        self.assertEqual(30.00, self.user.savings_goal)
