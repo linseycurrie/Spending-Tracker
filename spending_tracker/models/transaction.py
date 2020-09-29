bcbad67f05b67e5395d28a8791d429911e55fd8b
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2020-09-29 09:40:08 +0100

    Working App

--- spending_tracker/models/transaction.py
+++ spending_tracker/models/transaction.py
@@ -1,9 +1,10 @@
 
 class Transaction:
 
-    def __init__(self, amount, category, date, merchant, id=None):
+    def __init__(self, amount, category, date, merchant, user, id=None):
         self.amount = amount
         self.category = category
         self.date = date
         self.merchant = merchant
+        self.user = user
         self.id = id
