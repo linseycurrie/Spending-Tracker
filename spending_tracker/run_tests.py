dfb002b6d4fa6b9e252b21676d30fd0751f3c3f3
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2020-09-30 11:29:49 +0100

    Hand In

---
+++ spending_tracker/run_tests.py
@@ -0,0 +1,6 @@
+import unittest
+from tests.user_test import TestUser
+
+
+if __name__ == '__main__':
+    unittest.main()
--- spending_tracker/tests/user_test.py
+++ spending_tracker/tests/user_test.py
@@ -6,22 +6,24 @@ from models.user import User
 class TestUser(unittest.TestCase):
 
     def setUp(self):
-        self.user = User("John", 50.00)
-        self.user1 = User("Betty", 95.00)
-        self.user2 = User("Sue", 105.00)
+        self.user = User("John", 100.00)
         
 
     def test_alert_near_limit_Under_warning(self):
-        total = 100
-        self.assertEqual("ok", self.user.alet_near_limit(total))
+        total = 50
+        self.assertEqual("ok", self.user.alert_near_limit(total))
 
     def test_alert_near_limit_warning(self):
-        total = 100
-        self.assertEqual("warning", self.user1.alert_near_limit(total))
+        total = 105
+        self.assertEqual("warning", self.user.alert_near_limit(total))
 
-    def test_alert_near_limit_Under_over_limit(self):
+    def test_alert_near_limit_over_limit(self):
+        total = 150
+        self.assertEqual("over", self.user.alert_near_limit(total))
+
+    def test_alert_near_limit_over(self):
         total = 100
-        self.assertEqual("over", self.user2.alert_near_limit(total))
+        self.assertEqual("on", self.user.alert_near_limit(total))
 
     
 
