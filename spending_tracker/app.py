c2bb48d0792ef6f04572ce6fcbd78b3a38d34414
Linsey Currie <linseycurrie@Linseys-MacBook-Pro.local>
2021-01-13 20:53:32 +0000

    commit

--- spending_tracker/app.py
+++ spending_tracker/app.py
@@ -17,5 +17,6 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
 
